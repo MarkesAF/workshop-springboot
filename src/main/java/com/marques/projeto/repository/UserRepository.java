5bca7265c92c99b364f1d5100dbae19ff054381b
Augusto Marques <markesaf16@gmail.com>
2023-09-23 18:18:47 -0300

    Implementando a Camada Service

--- src/main/java/com/marques/projeto/repository/UserRepository.java
+++ src/main/java/com/marques/projeto/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.marques.projeto.repository;
 
 import com.marques.projeto.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
---
+++ src/main/java/com/marques/projeto/service/UserService.java
@@ -0,0 +1,25 @@
+package com.marques.projeto.service;
+
+
+import com.marques.projeto.entities.User;
+import com.marques.projeto.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+       return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
