6c1cafe342ddbc4122a5f88c5b08f011c2a55b36
Augusto Marques <markesaf16@gmail.com>
2023-09-24 15:58:25 -0300

    Criando exceções para FindById

---
+++ src/main/java/com/marques/projeto/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.marques.projeto.controllers.exceptions;
+
+import com.marques.projeto.service.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> notFound(ResourceNotFoundException e, HttpServletRequest req){
+        String error = "Resource Not Found";
+        HttpStatus http = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(),http.value(),error, e.getMessage(), req.getRequestURI());
+        return ResponseEntity.status(http).body(err);
+    }
+}
--- src/main/java/com/marques/projeto/service/UserService.java
+++ src/main/java/com/marques/projeto/service/UserService.java
@@ -3,6 +3,7 @@ package com.marques.projeto.service;
 
 import com.marques.projeto.entities.User;
 import com.marques.projeto.repository.UserRepository;
+import com.marques.projeto.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class UserService {
     }
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
